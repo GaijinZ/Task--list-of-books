6471bcb8c741187beec09338be9a5090836c70d1
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-16 19:54:20 +0100

    default value for thumbnail model field

--- list_of_books/models.py
+++ list_of_books/models.py
@@ -7,7 +7,8 @@ class Book(models.Model):
     published_date = models.CharField(max_length=20, null=True, blank=True)
     ISBN_number = models.BigIntegerField(null=True, blank=True)
     page_count = models.IntegerField(null=True, blank=True)
-    thumbnail = models.CharField(max_length=255, blank=True)
+    thumbnail = models.CharField(default='https://code-artisan.io/wp-content/uploads/2020/12'
+                                         '/default_book_cover_2015.jpg', max_length=255, blank=True)
     language = models.CharField(max_length=5, null=True, blank=True)
 
     def __str__(self):
