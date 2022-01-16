c19bd5dc1a412f113cc1f84969f137ef0edb23e7
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-16 19:30:09 +0100

    changed ISBN_number model field to big int field

--- list_of_books/models.py
+++ list_of_books/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     authors = models.CharField(max_length=200)
     published_date = models.CharField(max_length=20, null=True, blank=True)
-    ISBN_number = models.IntegerField(null=True, blank=True)
+    ISBN_number = models.BigIntegerField(null=True, blank=True)
     page_count = models.IntegerField(null=True, blank=True)
     thumbnail = models.CharField(max_length=255, blank=True)
     language = models.CharField(max_length=5, null=True, blank=True)
--- list_of_books/views.py
+++ list_of_books/views.py
@@ -115,9 +115,9 @@ class ImportToDBView(TemplateView):
             check_isbn_type = ''
 
             if book['volumeInfo']['industryIdentifiers'][0]['type'].lower() == 'other':
-                check_isbn_type += book['volumeInfo']['industryIdentifiers'][0]['identifier'].split(':')[1]
+                check_isbn_type = book['volumeInfo']['industryIdentifiers'][0]['identifier'].split(':')[1]
             else:
-                check_isbn_type += book['volumeInfo']['industryIdentifiers'][0]['identifier']
+                check_isbn_type = book['volumeInfo']['industryIdentifiers'][0]['identifier']
 
             Book.objects.update_or_create(title=book['volumeInfo']['title'],
                                           authors=', '.join([i for i in book['volumeInfo']
