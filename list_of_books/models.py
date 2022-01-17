3a869711a7778da27f927676dec536640c2d927e
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-17 19:37:43 +0100

    default book cover in models save method

--- list_of_books/models.py
+++ list_of_books/models.py
@@ -7,9 +7,14 @@ class Book(models.Model):
     published_date = models.CharField(max_length=20, null=True, blank=True)
     ISBN_number = models.BigIntegerField(null=True, blank=True)
     page_count = models.IntegerField(null=True, blank=True)
-    thumbnail = models.CharField(default='https://code-artisan.io/wp-content/uploads/2020/12'
-                                         '/default_book_cover_2015.jpg', max_length=255, blank=True)
+    thumbnail = models.CharField(max_length=255, blank=True)
     language = models.CharField(max_length=5, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.thumbnail:
+            self.thumbnail = 'https://code-artisan.io/wp-content/uploads/2020/12' \
+                             '/default_book_cover_2015.jpg'
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.authors} - {self.title}'
