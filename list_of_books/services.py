61185bbffa602ca8ebe08c2fa335fbbef07c8148
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-20 00:19:15 +0100

    changed requests function

--- list_of_books/services.py
+++ list_of_books/services.py
@@ -2,10 +2,9 @@ import requests
 
 
 def get_books(q_search, terms=None, term_search=None):
+    url = f'https://www.googleapis.com/books/v1/volumes?q={q_search}'
     if terms and term_search:
-        url = f'https://www.googleapis.com/books/v1/volumes?q={q_search}+{terms}:{term_search}'
-    else:
-        url = f'https://www.googleapis.com/books/v1/volumes?q={q_search}'
+        url = url + f'+{terms}:{term_search}'
     req = requests.get(url).json()
     books = req['items']
     return books
