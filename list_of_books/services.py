744d28c2532cde885b1b6cf3de6928a32a6b4fca
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-21 20:22:13 +0100

    deleted files

--- list_of_books/services.py
+++ list_of_books/services.py
@@ -4,7 +4,7 @@ import requests
 def get_books(q_search, terms=None, term_search=None):
     url = f'https://www.googleapis.com/books/v1/volumes?q={q_search}'
     if terms and term_search:
-        url = url + f'+{terms}:{term_search}'
+        url += f'+{terms}:{term_search}'
     req = requests.get(url).json()
     books = req['items']
     return books
